1865e8140c1e39055d3297ba052c48157e82d957
Mukadus Jawaid <muqadusjavaidsultan93@gmail.com>
2024-03-23 16:08:11 +0500

    added components and pages

---
+++ src/Pages/Favorites/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./Favorites.module.css";
+import { mergeClass } from "../../utils/helper";
+
+export default function Favorites() {
+  return <div></div>;
+}
---
+++ src/Pages/RegistrationForm/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./RegistrationForm.module.css";
+
+export default function RegistrationForm() {
+  return <div></div>;
+}
